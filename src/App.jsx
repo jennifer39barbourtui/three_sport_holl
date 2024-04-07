9b3faa4d32757afa0e3103c2c9914365fb064ada
Ihor Nepsha <jennifer39barbourtui@outlook.com>
2024-04-07 11:56:01 +0900

    feat: complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,68 @@
-import { useRef } from 'react'
-import { OrbitControls, TorusKnot } from '@react-three/drei'
-import { Canvas, useFrame } from '@react-three/fiber'
+import * as THREE from 'three'
+import { useLayoutEffect } from 'react'
+import { BakeShadows, CubeCamera, Environment, OrbitControls, TorusKnot, useBoxProjectedEnv, useGLTF } from '@react-three/drei'
+import { Canvas, applyProps } from '@react-three/fiber'
+import { useControls } from 'leva'
 
 export default function App() {
   return (
-    <Canvas style={{ position: 'fixed', inset: 0 }}>
-      <color attach="background" args={['black']} />
-      <OrbitControls />
-      <Thing />
+    <Canvas frameloop='demond' dpr={[1, 1.5]} shadows camera={{ near: 0.1, far: 40, fov: 75 }}>
+      <fog attach="fog" args={['purple', 0, 130]} />
+      <ambientLight intensity={0.1} />
+      <group position={[0, -1, 0]}>
+        <spotLight castShadow intensity={10} angle={0.1} position={[-200, 220, -100]} shadow-mapSize={[2048, 2048]} shadow-bias={-0.000001} />
+        <spotLight angle={0.1} position={[-250, 120, -200]} intensity={1} castShadow shadow-mapSize={[50, 50]} shadow-bias={-0.000001} />
+        <spotLight angle={0.1} position={[250, 120, 200]} intensity={1} castShadow shadow-mapSize={[50, 50]} shadow-bias={-0.000001} />
+        <Court />
+        <Floor />
+      </group>
+      <OrbitControls minPolarAngle={Math.PI / 2} maxPolarAngle={Math.PI / 2} />
+      <Environment files="/noon_grass_1k.hdr" background />
+      <BakeShadows />
     </Canvas>
   )
 }
 
-function Thing() {
-  const ref = useRef()
-  useFrame(() => (ref.current.rotation.y += 0.01))
+function Court(props) {
+  const { scene } = useGLTF('/court.glb')
+  useLayoutEffect(() => {
+    scene.traverse(o => {
+      if (o.isMesh) {
+        applyProps(o, {castShadow: true, receiveShadow: true, 'material-envMapIntensity': 0.1})
+      }
+    })
+    const floor = scene.getObjectByName('GymFloor_ParquetShader_0')
+    if (floor) floor.parent.remove(floor)
+  })
+  return <primitive object={scene} {...props} />
+}
+
+function Floor(props) {
+  const { nodes, materials } = useGLTF('/court.glb')
+  const { up, scale, ...config } = useControls({
+    up: { value: -0.5, min: -10, max: 10 },
+    scale: { value: 27, min: 0, max: 50 },
+    roughness: { value: 0.06, min: 0, max: 0.15, step: 0.001 },
+    envMapIntensity: { value: 1, min: 0, max: 5 }
+  })
+  const projection = useBoxProjectedEnv([0, up, 0], [scale, scale, scale])
   return (
-    <TorusKnot ref={ref} args={[1, 0.3, 128, 16]}>
-      <meshNormalMaterial />
-    </TorusKnot>
+    <CubeCamera frames={1} position={[0, 0.5, 0]} rotation={[0, 0, 0]} resolution={2048} near={1} far={1000} {...props}>
+      {(texture) => (
+        <mesh receiveShadow position={[-13.68, -0.467, 17.52]} scale={0.02} geometry={nodes.GymFloor_ParquetShader_0.geometry} dispose={null}>
+          <meshStandardMaterial
+            map={materials.ParquetShader.map}
+            normalMap={materials.ParquetShader.normalMap}
+            normalMap-encoding={THREE.LinearEncoding}
+            envMap={texture}
+            metalness={0.0}
+            normalScale={[0.25, -0.25]}
+            color="#aaa"
+            {...projection}
+            {...config}
+          />
+        </mesh>
+      )}
+    </CubeCamera>
   )
 }
